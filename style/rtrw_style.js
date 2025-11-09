840d8b8864f37cae172bfafd821575f422c55bfb
hanggeraqiimmohpandego-sys <hanggeraqiimmohpandego@gmail.com>
2025-11-09 11:24:42 +0700

    Add files via upload

---
+++ style/rtrw_style.js
@@ -0,0 +1,23 @@
+// style/rtrw_style.js
+// 🎨 Warna simbolisasi RTRW berdasarkan kolom NAMOBJ
+const rtrwStyleByNama = {
+    "Badan Air": "rgb(151,219,242)",
+    "Kawasan Cagar Budaya": "rgb(255,55,205)",
+    "Kawasan Hutan Adat": "rgb(5,105,40)",
+    "Kawasan Hutan Produksi": "rgb(0,155,55)",
+    "Kawasan Konservasi": "rgb(120,60,205)",
+    "Kawasan Lindung Geologi": "rgb(120,135,130)",
+    "Kawasan Pariwisata": "rgb(255,165,255)",
+    "Kawasan Pembuangan Hasil Pengerukan di Laut": "rgb(215,190,155)",
+    "Kawasan Pencadangan Konservasi di Laut": "rgb(90,150,150)",
+    "Kawasan Pergaraman": "rgb(180,150,120)",
+    "Kawasan Perikanan": "rgb(80,125,210)",
+    "Kawasan Perlindungan Setempat": "rgb(5,215,215)",
+    "Kawasan Permukiman": "rgb(255,125,0)",
+    "Kawasan Pertahanan dan Keamanan": "rgb(155,0,255)",
+    "Kawasan Pertambangan dan Energi": "rgb(5,25,55)",
+    "Kawasan Pertanian": "rgb(200,200,60)",
+    "Kawasan Peruntukan Industri": "rgb(105,0,0)",
+    "Kawasan Transportasi": "rgb(215,55,0)",
+    "Kawasan yang Memberikan Perlindungan terhadap Kawasan Bawahannya": "rgb(35,65,40)"
+};
